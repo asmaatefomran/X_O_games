6810bef6ec074a1a884af3c4cdd8827dedaedc27
Asmaa Atef <asmaatef6789@gmail.com>
2023-12-17 01:24:30 +0200

    Update X_O_App.cpp

--- X_O_App.cpp
+++ X_O_App.cpp
@@ -5,41 +5,80 @@
 
 #include <iostream>
 #include"../include/BoardGame_Classes.hpp"
+#include"BoardGame_Classes.hpp"
+#include "Player.cpp"
+#include "PyramicPlayer.cpp"
+#include "FourinRowPlayer.cpp"
+#include "FiveXFivePlayer.cpp"
+#include"GameManager.cpp"
+#include"RandomPlayer.cpp"
+#include"Pyramic_Board.cpp"
+#include "Four_in_a_row_X_O_Board.cpp"
+#include "TICTAC_X_O_Board.cpp"
 using namespace std;
 
 int main() {
     cout<<"Welcome to the Board Game App!\n"<<"Choose a game(^_^):\n"
-        <<"1.Four in a row\n"
-        <<"2.X_O\n";
+        <<"1.Pyramic\n"
+        <<"2.Four in a row\n"
+        <<"3.X_O\n";
+    
     int gameChoice;
     cin>>gameChoice;
-    if(gameChoice==1||gameChoice==2){
-        Board* gameBoard;
-
-
-        Player* players[2];
-        players[0] = new Player (1, 'x');
+    if(gameChoice==1){
+        Pyramic_Board* gameBoard;
+        PyramicPlayer* players[2];
+        players[0] = new PyramicPlayer (1, 'x');
        // players[0]->get_move(1 ,'X',gameChoice);
-        cout << "Welcome to FCAI X-O Game. :)\n";
+        cout << "Welcome to FCAI Pyramic x_o Game. :)\n";
         cout << "Press 1 if you want to play with computer: ";
         int choice;
         cin >> choice;
         if (choice != 1)
-            players[1] = new Player (2, 'o');
+            players[1] = new PyramicPlayer (2, 'o');
         else
             //Player pointer points to child
             players[1] = new RandomPlayer ('o', 3);
 
-        if(gameChoice==1){
-            gameBoard=new Four_in_a_row_X_O_Board();
-
-        }else
-        {
-           gameBoard=new X_O_Board();
+        gameBoard=new Pyramic_Board();
 
         }
+    else if(gameChoice==2){
+        Four_in_a_row_Board* gameBoard;
+        FourinRowPlayer* players[2];
+        players[0] = new FourinRowPlayer (1, 'x');
+       // players[0]->get_move(1 ,'X',gameChoice);
+        cout << "Welcome to FCAI Pyramic x_o Game. :)\n";
+        cout << "Press 1 if you want to play with computer: ";
+        int choice;
+        cin >> choice;
+        if (choice != 1)
+            players[1] = new FourinRowPlayer (2, 'o');
+        else
+            //Player pointer points to child
+            players[1] = new RandomPlayer ('o', 3);
 
+        gameBoard=new Four_in_a_row_Board();
+ 
+    }
+    else if(gameChoice==3){
+       TICTAC_X_O_Board* gameBoard;
+        FoveXFivePlayer* players[2];
+        players[0] = new FoveXFivePlayer (1, 'x');
+       // players[0]->get_move(1 ,'X',gameChoice);
+        cout << "Welcome to FCAI Pyramic x_o Game. :)\n";
+        cout << "Press 1 if you want to play with computer: ";
+        int choice;
+        cin >> choice;
+        if (choice != 1)
+            players[1] = new FoveXFivePlayer (2, 'o');
+        else
+            //Player pointer points to child
+            players[1] = new RandomPlayer ('o', 3);
 
+        gameBoard=new TICTAC_X_O_Board(); 
+    }
+        
 
         GameManager* x_o_game=new GameManager(gameBoard, players);
         x_o_game->run();
@@ -47,13 +86,6 @@ int main() {
         delete players[1];
         delete gameBoard;
         delete x_o_game;
-    }
-else{
-    cout<<"Invaild choice"<<" "<<"Exiting....\n";
-
-}
-
-
 
     system ("pause");
  return 0;
